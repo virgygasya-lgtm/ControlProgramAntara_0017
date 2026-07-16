5848f749e01feb84dd12e8809233782258b8e64b
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-07-16 14:17:16 +0700

    menambahkanfungsistatuskelulusandengankategori.cpp

---
+++ menambahkanfungsistatuskelulusandengankategori.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+float Rata_rata(float a, float b){
+    return (a+b)/2;
+}
+
+string Status_lulus(float r){
+    if (r >= 60)
+        if(r > 80){
+            return "Lulus Cumlade";
+        }
+        else{
+            return "Lulus Baik";
+        }
+    else
+        return "Gagal";
+}
