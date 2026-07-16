7570edd5f82dcf82548e2d325bd9f1eb2c59b26e
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-07-16 14:18:12 +0700

    menambahkaninputnilaibahasainggrisdanmatematika.cpp

--- menambahkaninputnilaibahasainggrisdanmatematika.cpp
+++ menambahkaninputnilaibahasainggrisdanmatematika.cpp
@@ -16,3 +16,14 @@ string Status_lulus(float r){
     else
         return "Gagal";
 }
+
+int main(){
+
+    float nilBI, nilMT, rerata;
+    string status;
+
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilMT;
