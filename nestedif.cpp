82b000d9868c6b100e3e873990af1ee94d6152a4
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-07-16 14:22:13 +0700

    nestedif.cpp

--- nestedif.cpp
+++ nestedif.cpp
@@ -28,9 +28,16 @@ int main(){
     cout << "Masukkan nilai Matematika = ";
     cin >> nilMT;
 
-        rerata = Rata_rata(nilBI, nilMT);
+    //rerata = (nilBI+nilMT)/2;
+    //if (rerata >= 60)
+    //    status = "Lolos";
+    //else
+    //    status = "Gagal";
 
-            cout << "Status kelulusan = "
+    rerata = Rata_rata(nilBI, nilMT);
+    //status = Status_lulus(rerata);
+
+    cout << "Status kelulusan = "
          << Status_lulus(rerata)
          << ",dengan nilai rerata = "
          << rerata << endl;
